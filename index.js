3a09d84947e1ea0541668b48f9f2c2a0397fe69f
RaulRLM <160418382+RaulRLM@users.noreply.github.com>
2025-02-10 10:37:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const express = require('express')
-const mysql = require('mysql2')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 require('dotenv').config()
 const swaggerJsdoc = require('swagger-jsdoc')
 const swaggerUi = require('swagger-ui-express')
 
-// Importam les configuracions
+// Importamos la configuración de la BD
 const db = require('./config/db')
-const userRoutes = require('./routes/userRoutes') // Importar las rutas
-const plantRoutes = require('./routes/plantRoutes') // Importar las rutas de plantas
-const itemsRoutes = require('./routes/itemsRoutes') // Importa las rutas de los ítems
+
+// Importamos las rutas
+const userRoutes = require('./routes/userRoutes')
+const plantRoutes = require('./routes/plantRoutes')
+const itemsRoutes = require('./routes/itemsRoutes')
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT
 
 // Configuración de Swagger
 const swaggerOptions = {
@@ -22,20 +23,18 @@ const swaggerOptions = {
       title: 'API Juego de Plantas',
       version: '1.0.0',
       description:
-        'Documentació de la API per a gestionar plantes, usuaris, ítems, etc.',
+        'Documentación de la API para gestionar plantas, usuarios, ítems, etc.',
     },
     servers: [
       {
-        url: 'http://localhost:3000',
+        url: process.env.API_URL || 'https://apiplantes09.azurewebsites.net', // URL en Azure
       },
     ],
   },
-  apis: ['./src/routes/*.js'], // Rutas de tus controladores
+  apis: ['./src/routes/*.js'],
 }
 
 const swaggerDocs = swaggerJsdoc(swaggerOptions)
-
-// Rutas para servir la documentación Swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 // Configuración de middleware
@@ -43,12 +42,17 @@ app.use(cors())
 app.use(bodyParser.json())
 
 // Rutas
-app.use('/usuaris', userRoutes) // Ruta para usuarios
-app.use('/plantas', plantRoutes) // Ruta para plantas
-app.use('/items', itemsRoutes) // Rutas de ítems
-app.use('/items_usuaris', itemsRoutes) // Rutas de ítems
+app.use('/usuaris', userRoutes)
+app.use('/plantas', plantRoutes)
+app.use('/items', itemsRoutes)
+
+// Middleware de manejo de errores global
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).json({ message: 'Error interno del servidor' })
+})
 
 // Inicia el servidor
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`)
+  console.log(`✅ Servidor corriendo en el puerto ${PORT}`)
 })
